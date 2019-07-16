6982dc112ec325c2db8c943d7043822babfcd907
Duc Tran <tv.duc95@gmail.com>
2019-07-16 07:03:04 +0000

    Revert "Add css-loader, file-loader"
    This reverts commit 95a59391c8662c85a59a1bc326c65830713d538e

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,25 +1,12 @@
-const path = require('path');
-
-module.exports = {
-  module: {
-    rules: [{
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        include: [
-          path.resolve(__dirname, "node_modules/icheck")
-        ]
-      },
-      {
-        test: /\.(png|jp(e*)g|svg)$/,
-        loader: 'file-loader',
-        options: {
-          publicPath: 'assets',
-          // name: 'assets/lib/icheck/skins/[name].[ext]',
-        }
-        // include: [
-        //   path.resolve(__dirname, "node_modules/icheck")
-        // ]
-      }
-    ]
-  }
+const webpack = require('webpack');
+const pkg = require('./package.json');
+ 
+module.exports = (config, options) => {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'APP_VERSION': JSON.stringify(pkg.version),
+    }),
+  );
+ 
+  return config;
 };
